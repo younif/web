cefe92c34c09fd7371293f02f218a4f1e19beb2d
youni <wosmwu@q>
2022-06-17 11:15:40 +0800

    bench and test

---
+++ benchmark/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by youni on 2022/6/17.
+//
+using namespace std;
+
+int main(int argc, char** argv) {
+
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
     Server server("my_server", InetAddress(8888));
     server.run();
 }
